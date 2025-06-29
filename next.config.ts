6e3bf7127a53dbcc91accce4fec296a10a6f3125
Deepak Kamboj <deepakkamboj@gmail.com>
2025-06-29 15:11:28 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath:
+    process.env.NODE_ENV === "production" ? "/vercel-agentic-rag-starter" : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/vercel-agentic-rag-starter/" : "",
 };
 
 export default nextConfig;
