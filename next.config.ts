bafbe20fa27d34f3c75a4ac0e64e139a3fae7330
Deepak Kamboj <deepakkamboj@gmail.com>
2025-06-29 18:19:25 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,29 @@
-import { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
-const config: NextConfig = {  
-  basePath: process.env.BASEPATH,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack']
-    });
+const nextConfig: NextConfig = {
+  /**
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
 
-    return config;
+  /**
+   * Set base path. This is usually the slug of your repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/nextjs-github-pages",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
   },
-  output: 'export',
 };
 
-export default config;
+export default nextConfig;
